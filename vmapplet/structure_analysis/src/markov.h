a190a739dc6ddeb1f5cf1c7d2d6fbeb2cba6d7c7
jvail <6503378+jvail@users.noreply.github.com>
2022-04-08 12:05:43 +0200

    Fix code and add cmake files

---
+++ vmapplet/structure_analysis/src/markov.h
@@ -0,0 +1,9 @@
+#ifndef MARCOV_H
+#define MARCOV_H
+
+#include "sequence_analysis/semi_markov.h"
+#include "sequence_analysis/hidden_semi_markov.h"
+#include "sequence_analysis/sequences.h"
+#include "stat_tool/stat_tools.h"
+
+#endif
